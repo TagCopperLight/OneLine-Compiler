81f8e60468803c61ab5f210384a32bc4d6a7953a
TagCopperLight <tagcopperlight@gmail.com>
2022-10-06 12:09:27 +0200

    Tokens added

---
+++ data/token.py
@@ -0,0 +1,75 @@
+from enum import Enum
+from re import compile
+
+class Token(EQEnum):
+    # Specials
+    ILLEGAL = 'ILLEGAL'
+    EOF = 'EOF'
+
+    # Data types
+    STRING = compile("")
+    INT = compile("")
+    FLOAT = compile("")
+    
+    # Brackets
+    L_ROUND = compile("")
+    R_ROUND = compile("")
+    L_SQUARE = compile("")
+    R_SQUARE = compile("")
+    L_CURLY = compile("")
+    R_CURLY = compile("")
+
+    # Operators
+    PLUS = compile("")
+    MINUS = compile("")
+    MULT = compile("")
+    DIVIDE = compile("")
+    DIVIDE_INT = compile("")
+    MODULO = compile("")
+    POWER = compile("")
+
+    # Logical Operators
+    AND = compile("")
+    OR = compile("")
+    NOT = compile("")
+
+    # Conditionnal Operators
+    EQ = compile("")
+    NEQ = compile("")
+    LOWER = compile("")
+    HIGHER = compile("")
+    EQLOWER = compile("")
+    EQHIGHER = compile("")
+
+    # Keywords
+    TRUE = compile("")
+    FALSE = compile("")
+    NONE = compile("")
+    IF = compile("")
+    ELSE = compile("")
+    FOR = compile("")
+    WHILE = compile("")
+    DEF = compile("")
+    RETURN = compile("")
+    PRINT = compile("")
+
+    # Variables
+    ID = compile("")
+
+    # Comments
+    COMMENT = compile("")
+
+    # Delimiters
+    COMMA = compile("")
+    WHITESPACE = compile("")
+
+
+class EQEnum(Enum):
+    def __eq__(self, element) -> bool:
+        if is_instance(b, str):
+            return self.name == element
+        else:
+            return self.name == element.name
+    
+    def __hash__(self):
+        return id(self.name)
